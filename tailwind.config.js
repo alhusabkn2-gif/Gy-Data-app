a869498b22073ac5d861076025af8f665b0d617d
alhusabkn2-gif <alhusabkn2@gmail.com>
2026-08-14 20:54:48 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        // Muna canja ainihin kalolin Tailwind domin duk shafin da ke amfani da bg-gray ko bg-slate ya dawo kalar annashuwa kai tsaye
-        gray: {
-          50: '#F4FAF8',
-          100: '#E6F4F1',
-          900: '#0F172A',
-        },
-        slate: {
-          50: '#F4FAF8',
-          100: '#E6F4F1',
-          900: '#0F172A',
-        },
         brand: {
-          bg: '#F4FAF8',
-          primary: '#0D9488',
-          accent: '#F97316',
+          cyan: '#00BCDF',     // Cyan dake saman Welcome Header & Buttons
+          green: '#74C856',    // Koren Buy Airtime card da Go to Home button
+          orange: '#FF914D',   // Orange dake kasan background shapes
+          lightGreen: '#EBF9E8', // Light green background na Buy Airtime
+          lightOrange: '#FFF1E8',// Light orange background na Transaction History
         }
-      },
+      }
     },
   },
   plugins: [],
-};
+}
